a4e2c193f6472e564b9c8bf8bc59ef12c0f96c3b
Rico Konieczek <rico.konieczek@gmail.com>
2015-04-26 11:40:30 +0200

    Move MainActivity.PlaceholderFragment to DetailFragment

--- app/src/main/java/de/kunee/divelog/MainActivity.java
+++ app/src/main/java/de/kunee/divelog/MainActivity.java
@@ -1,13 +1,9 @@
 package de.kunee.divelog;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -18,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.main_activity);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new MainFragment())
                     .commit();
         }
     }
@@ -44,21 +40,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.main_fragment, container, false);
-            return rootView;
-        }
     }
 }
